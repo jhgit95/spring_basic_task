6f938e20275bcf892ac5c531ffbb5055fc1f4f7d
Juho <ljlh9509@gmail.com>
2024-08-11 13:58:56 +0900

    first commit

---
+++ src/main/java/com/basic/spring_basic_task/SpringBasicTaskApplication.java
@@ -0,0 +1,13 @@
+package com.basic.spring_basic_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/basic/spring_basic_task/SpringBasicTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.spring_basic_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
