b52076837797e99c6739c00be0526ba6b1d28a14
Juho <ljlh9509@gmail.com>
2024-08-16 22:43:30 +0900

    [FIX] URL을 RESTful하게 수정~

--- src/main/java/com/basic/spring_basic_task/controller/ScheduleController.java
+++ src/main/java/com/basic/spring_basic_task/controller/ScheduleController.java
@@ -16,35 +16,35 @@ public class ScheduleController {
     private ScheduleService scheduleService;
 
     // 할 일 전부 조회
-    @GetMapping("/get")
+    @GetMapping()
     public ResponseEntity<List<ScheduleResponseDto>> getAllSchedules() {
         return ResponseEntity.status(200).body(scheduleService.getAllSchedules());
     }
 
 
     // 일정 등록
-    @PostMapping("/post")
+    @PostMapping()
     public ResponseEntity<ScheduleSingleDto> postSchedule(@RequestBody ScheduleAddDto AddDto) {
         return ResponseEntity.status(200).body(scheduleService.postSchedule(AddDto));
     }
 
 
     // 할 일 단일 조회
-    @GetMapping("/get/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<ScheduleSingleDto> getSingleSchedule(@PathVariable int id) {
         return ResponseEntity.status(200).body(scheduleService.getSingleSchedule(id));
     }
 
 
     // 담당자와 수정일로 검색
-    @GetMapping("/get/search")
+    @GetMapping("/search")
     public ResponseEntity<List<ScheduleResponseDto>> getScheduleSearch(@RequestBody ScheduleSearchDto searchDto) {
         return ResponseEntity.status(200).body(scheduleService.getScheduleSearch(searchDto));
     }
 
 
     // 일정 수정
-    @PutMapping("/get/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<String> updateSchedule(@RequestBody ScheduleRequestDto sReqDto) {
         if (scheduleService.updateSchedule(sReqDto) == 1) {
             return ResponseEntity.status(200).body(scheduleService.getSingleSchedule(sReqDto.getScheduleId()).data());
@@ -55,7 +55,7 @@ public class ScheduleController {
 
 
     // 선택한 일정 삭제
-    @DeleteMapping("/delete")
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteSchedule(@RequestBody ScheduleRequestDto sReqDto) {
         if (scheduleService.deleteSchedule(sReqDto) == 1) {
             return ResponseEntity.status(200).body("삭제 완료");
@@ -65,7 +65,7 @@ public class ScheduleController {
     }
 
     // 페이지네이션
-    @GetMapping("/get/pagination")
+    @GetMapping("/pagination")
     public List<ScheduleResponseDto> getPaginationSchedules(
             @RequestParam(required = false) Integer page,
             @RequestParam(required = false) Integer size
